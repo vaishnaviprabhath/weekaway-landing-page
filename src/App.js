6fe571e2d5d5150cfa572958ca26d4003df5c540
vaishnaviprabhath <chvkarmy@gmail.com>
2023-02-15 09:35:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from 'react'
 import TopBar from './components/TopBar'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import Activities from './components/Activities'
+import Booking from './components/Booking'
+import Gallery from './components/Gallery'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <TopBar/>
       <NavBar />
+      <Hero/>
+      <Activities />
+      <Booking />
+      <Gallery />
+      <Contact />
+      <Footer />
     </div>
   );
 }
