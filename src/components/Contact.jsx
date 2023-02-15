db0ddf9203620c9b18622defd687ffaeb58a370a
Vaishnavi Prabhath <chvkarmy@gmail.com>
2023-02-15 09:27:17 +0530

    added contacts

---
+++ src/components/Contact.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Contact =() => {
+    return (
+        <div id= 'contact' className ='max-w-[1140px] m-auto w-full p-4 py-16'>
+
+            <h2 className='text-center text-gray-700'>Send us a message</h2>
+            <p className ='text-center text-gray-700 py-2'>We're standing by!</p>
+            <div className='grid md:grid-cols-2'>
+            <div>
+                <img src="https://images.unsplash.com/photo-1507237615867-0d4d2ad6b2d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzc3fHx2YWNhdGlvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1035&q=80" alt="/"
+                className='w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]'
+                
+                
+                />
+                <form>
+                    <div className='grid grid-cols-2'>
+                        <input className='border m-2 p-2'  type="text" placeholder="First"/>
+                        <input className='border m-2 p-2'  type="text" placeholder="Last"/>
+                        <input className='border m-2 p-2' type="email" placeholder="Email"/>
+                        <input className='border m-2 p-2' type="tel" placeholder="Phone"/>
+                        <input className='border col-span-2 m-2 p-2'type="text" placeholder="Address"/>
+                        <textarea  className='border col-span-2 m-2 p-2'cols="20" rows="5"></textarea>
+                        <button className='col-span-2 m-2'>Submit</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+      </div>
+    )
+}
+
+
+export default Contact
