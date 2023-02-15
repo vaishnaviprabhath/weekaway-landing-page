c2959fbfa6c17cfc0f923d5698ff914cf428b89f
Vaishnavi Prabhath <chvkarmy@gmail.com>
2023-02-15 09:22:17 +0530

    added images

---
+++ src/components/Activities.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Activities = () => {
+    return (
+        <div className=' grid max-w-[1140px] m-aut0 w-full md:flex mt-[-75px]'>
+            <div className='relative p-4'>
+                <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+                <img
+                    className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+                    src="https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDBBMHxwaG90by1wYWdlfHxBfGVufDBBHx8&auto=format&fit=crop&w=2070&q=80" alt="/" />
+            </div>
+            <div className='relative p-4'>
+                <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+                <img
+                    className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+                    src="https://images.unsplash.com/photo-1540541338287-41700207dee6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="/" />
+            </div>
+            <div className='relative p-4'>
+                <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+                <img
+                    className='w-full h-full object-cover relative border-4 border-white shadow-lg'
+                    src="https://images.unsplash.com/photo-1623718649591-311775a30c43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="/" />
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Activities
